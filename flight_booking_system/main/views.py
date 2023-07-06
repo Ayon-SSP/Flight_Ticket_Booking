78f6a32cb759654feca5cc464512495845afa338
ayon_ssp <ayon.ssp@gmail.com>
2023-07-06 23:15:04 +0530

    feat: render page if login

--- flight_booking_system/main/views.py
+++ flight_booking_system/main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
 
 
-# Create your views here.
+
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
 
