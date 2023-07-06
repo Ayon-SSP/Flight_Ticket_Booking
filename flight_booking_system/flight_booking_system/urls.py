1f1536c3189b7cc749e62ecd511fe68deb807fac
ayon_ssp <ayon.ssp@gmail.com>
2023-07-07 00:35:49 +0530

    feat: Full Auth, basic elements mode

--- flight_booking_system/flight_booking_system/urls.py
+++ flight_booking_system/flight_booking_system/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = "✈️ Flight Booking Admin"
+admin.site.site_title = "✈️ Flight Booking Admin Portal"
+admin.site.index_title = "Welcome to ✈️ Flight Booking Researcher Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
