aa6b75b0e8762d8ca852ad98a6000ed62e8309f7
ayon_ssp <ayon.ssp@gmail.com>
2023-07-06 22:08:16 +0530

    feat: authentication added

--- flight_booking_system/flight_booking_system/settings.py
+++ flight_booking_system/flight_booking_system/settings.py
@@ -143,5 +143,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# LOGIN_REDIRECT_URL = '/home'
-# LOGOUT_REDIRECT_URL = '/login'
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
