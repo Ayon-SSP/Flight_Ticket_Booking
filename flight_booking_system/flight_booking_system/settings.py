e0b85934fbc432ebaaecd586ea76eec21305311b
ayon_ssp <ayon.ssp@gmail.com>
2023-07-06 21:33:27 +0530

    feat: added login

--- flight_booking_system/flight_booking_system/settings.py
+++ flight_booking_system/flight_booking_system/settings.py
@@ -46,9 +46,13 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
 
-    'users',
+    # 'users',
     'main.apps.MainConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -138,3 +142,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN_REDIRECT_URL = '/home'
+# LOGOUT_REDIRECT_URL = '/login'
