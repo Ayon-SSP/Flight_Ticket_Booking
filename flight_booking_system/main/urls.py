be1a2e3ad99948498b3cbbcb87c53216db236ada
ayon_ssp <ayon.ssp@gmail.com>
2023-07-07 15:33:24 +0530

    feat: flight_list, add & remove flight, book_flight, user_booking and book flight

--- flight_booking_system/main/urls.py
+++ flight_booking_system/main/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('sign-up', views.sign_up, name='sign-up'),
     path('add', views.add_flight, name='add_flight'),
     path('remove/<int:flight_id>/', views.remove_flight, name='remove_flight'),
+    path('flights', views.flight_list, name='flight_list'),
+    # path('book/<int:flight_id>', views., name=''),
+    path('book/<int:flight_id>/', views.book_flight, name='book_flight'),
+    path('user_bookings/<str:user_id>/', views.user_bookings, name='user_bookings'),
 ]
--- flight_booking_system/main/views.py
+++ flight_booking_system/main/views.py
@@ -1,8 +1,10 @@
+import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
 from .models import Flight
+from django.db.models import Q
 
 @login_required(login_url='/login')
 def home(request):
@@ -21,6 +23,46 @@ def sign_up(request):
 
     return render(request, 'registration/sign_up.html', {"form": form})
 
+
+@login_required(login_url='/login')
+def flight_list(request):
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    if start_date and end_date:
+        flights = Flight.objects.filter(
+            Q(departure_time__gte=start_date) &
+            Q(departure_time__lte=end_date)
+        ).order_by('departure_time')
+        # print(flights)
+    else:
+        flights = Flight.objects.order_by('departure_time')
+
+    return render(request, 'main/flight_list.html', {'flights': flights})
+
+@login_required(login_url='/login')
+def book_flight(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    seats = flight.seats
+
+    available_seats = {i+1: seats[i] is -1 for i in range(len(seats)) if seats[i] is -1}
+    print(available_seats)
+    if request.method == 'POST':
+        seat_number = int(request.POST['seat_number'])
+        user_id = request.POST['user_id']
+        print(seat_number,user_id)
+
+        if seat_number in available_seats and available_seats[seat_number]:
+            seats[seat_number-1] = user_id
+            flight.save()
+            return redirect('view_bookings', flight_id=flight_id)
+
+    return render(request, 'main/flight_list.html', {'flight': flight, 'available_seats': available_seats})
+
+
+
+def user_bookings(request, user_id):
+    bookings = Flight.objects.filter(seats__contains=[user_id])
+    return render(request, 'main/user_bookings.html', {'bookings': bookings})
 
 
 def add_flight(request):
